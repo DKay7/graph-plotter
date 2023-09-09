5c6505896a65de0600a3bee6f6d0628dece34fe8
Daniil <kalinin.de@phystech.edu>
2023-09-09 03:56:00 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+def process_cmd_args():
+    pass
+
+def read_file_data():
+    pass
+
+def plot_graph(array_of_pairs):
+    graph = nx.from_np_array()
+
+if __name__ == "__main__":
+    process_cmd_args()
